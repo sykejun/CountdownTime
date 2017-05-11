8a1a4e7a23eb4af6cdec40f058c987c8ce7c7aa7
kejun <kejun@chinayie.com>
2017-05-11 13:25:17 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.administrator.test;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout countDown;
+    // 倒计时
+    private TextView daysTv, hoursTv, minutesTv, secondsTv;
+    private long mDay = 10;
+    private long mHour = 10;
+    private long mMin = 30;
+    private long mSecond = 00;// 天 ,小时,分钟,秒
+    private boolean isRun = true;
+    private Handler timeHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what==1) {
+                computeTime();
+                daysTv.setText(mDay+"");
+                hoursTv.setText(mHour+"");
+                minutesTv.setText(mMin+"");
+                secondsTv.setText(mSecond+"");
+                if (mDay==0&&mHour==0&&mMin==0&&mSecond==0) {
+                    countDown.setVisibility(View.GONE);
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countDown = (RelativeLayout) findViewById(R.id.countdown_layout);
+        daysTv = (TextView) findViewById(R.id.days_tv);
+        hoursTv = (TextView) findViewById(R.id.hours_tv);
+        minutesTv = (TextView) findViewById(R.id.minutes_tv);
+        secondsTv = (TextView) findViewById(R.id.seconds_tv);
+
+        startRun();
+    }
+
+    /**
+     * 开启倒计时
+     */
+    private void startRun() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (isRun) {
+                    try {
+                        Thread.sleep(1000); // sleep 1000ms
+                        Message message = Message.obtain();
+                        message.what = 1;
+                        timeHandler.sendMessage(message);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 倒计时计算
+     */
+    private void computeTime() {
+        mSecond--;
+        if (mSecond < 0) {
+            mMin--;
+            mSecond = 59;
+            if (mMin < 0) {
+                mMin = 59;
+                mHour--;
+                if (mHour < 0) {
+                    // 倒计时结束
+                    mHour = 23;
+                    mDay--;
+                }
+            }
+        }
+    }
+}
